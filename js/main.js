05c6b76267a80e19c4aa7c70af005d685fb731e1
Chris <81439582+Gromhork@users.noreply.github.com>
2021-12-11 14:19:09 +0100

    New animation for: - element out of view - navigation bar

--- js/main.js
+++ js/main.js
@@ -6,28 +6,36 @@ let scrollElements
 function elementInView(el, offset = 0) {
     let elementTop = el.getBoundingClientRect().top
 
-    return (elementTop <= $(window).innerHeight() - offset)
+    return (elementTop <= $(window).innerHeight() / offset)
 }
 
 function elementOutOfView(el, offset) {
     let elementTop = el.getBoundingClientRect().top
 
-    return (elementTop > $(window).innerHeight() - offset)
+    return (elementTop > $(window).innerHeight() / offset)
 }
 
 function displayScrollElement(el) {
-    $(el).addClass("scrolled")
+    $(el).removeClass("unScrolled")
+    setTimeout(function () {
+        $(el).addClass("scrolled")
+        $(el).css("opacity", "1")
+    }, 0.0002)
 }
 
 function hideScrollElement(el) {
     $(el).removeClass("scrolled")
+    setTimeout(function () {
+        $(el).addClass("unScrolled")
+        $(el).css("opacity", "0")
+    }, 0.00002)
 }
 
 function handleScrollAnimation() {
     scrollElements.forEach(element => {
-        if (elementInView(element, 50)) {
+        if (elementInView(element, 1.6)) {
             displayScrollElement(element)
-        } else if (elementOutOfView(element, 10)) { //Fade out animation
+        } else if (elementOutOfView(element, 1.4)) {
             hideScrollElement(element)
         }
     })
@@ -37,6 +45,7 @@ function handleScrollAnimation() {
 /*-------------------|Events|-------------------*/
 $(document).ready(function () {
     scrollElements = $(".js-scroll").toArray()
+    handleScrollAnimation()
 
     $(window).on("scroll", function () {
         handleScrollAnimation()
